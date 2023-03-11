753f75448cf5ecf843765189670559494b4dbbc3
Bryan Pino Acuña <br.pinoa@gmail.com>
2023-03-11 17:07:53 -0300

    Clase Startup

---
+++ WSVENTA1/WSVENTA1/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace WSVENTA1
+{
+    public static class Startup
+    {
+        public static WebApplication InicializarApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
+            var app = builder.Build();
+            Configure(app);
+            return app;
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+        }
+        private static void Configure(WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+           
+
+        }
+    }
+}
