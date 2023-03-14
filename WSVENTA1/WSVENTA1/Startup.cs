cbdaf8f374540c1e6e5cc5b1256f4750fb0b90c8
Bryan Pino Acuña <br.pinoa@gmail.com>
2023-03-14 13:09:16 -0300

    Re commit, new start
    Correction fix

--- WSVENTA1/WSVENTA1/Startup.cs
+++ WSVENTA1/WSVENTA1/Startup.cs
@@ -1,17 +1,7 @@
-﻿using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-namespace WSVENTA1
+﻿namespace WSVENTA1
 {
     public static class Startup
     {
-        static readonly string MiCors = "MiCors";
-
-
         public static WebApplication InicializarApp(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -28,18 +18,6 @@ namespace WSVENTA1
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            builder.Services.AddCors(Options =>
-            {
-                Options.AddPolicy(name: MiCors,
-                                        builder =>
-                                        {
-                                            builder.WithOrigins("*");
-
-                                        });
-            }) ;
-
-            builder.Services.AddControllers();
-
         }
         private static void Configure(WebApplication app)
         {
@@ -55,8 +33,6 @@ namespace WSVENTA1
             app.UseAuthorization();
 
             app.MapControllers();
-
-            app.UseCors(MiCors);
 
            
 
